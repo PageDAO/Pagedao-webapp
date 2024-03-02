54e5fccdea6282d24f63578ed99fbabb4d902d80
wipphil <84144261+wipphil@users.noreply.github.com>
2024-03-02 11:52:15 -0700

    Updated home page copy

--- src/template/Login.jsx
+++ src/template/Login.jsx
@@ -17,7 +17,7 @@ function Login() {
                         <div className="basis-2/3 py-40 justify-start items-center gap-20 flex-col">
                             <div className="flex-col justify-start items-start gap-6 inline-flex">
                                 <div className="text-neutral-50 text-6xl font-bold font-['Arvo']">
-                                    The platform for managing your book collections & selling your creations.
+                                    Open source publishing software to bring the world's literature to the world's people.
                                 </div>
                                 
                             </div>
